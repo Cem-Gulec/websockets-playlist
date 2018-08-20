1352c6d2f4d0a3a38504ec44ffc9b869c1855336
Cem-Gulec <35612479+Cem-Gulec@users.noreply.github.com>
2018-08-20 19:51:06 +0300

    Update chat.js

--- public/chat.js
+++ public/chat.js
@@ -6,27 +6,52 @@ var message = document.getElementById('message'),
       handle = document.getElementById('handle'),
       btn = document.getElementById('send'),
       output = document.getElementById('output'),
-      feedback = document.getElementById('feedback');
+      feedback =document.getElementById('feedback');
+      clear =document.getElementById('clear');
 
 // Emit events
 btn.addEventListener('click', function(){
-    socket.emit('chat', {
+  socket.emit('chat', {
+      message: message.value,
+      handle: handle.value
+  });
+  message.value = "";
+});
+
+
+message.addEventListener('keypress',function(event){
+  socket.emit('typing',handle.value);
+});
+
+message.addEventListener('keyup', function(event) {
+
+  // Number 13 is the "Enter" key on the keyboard
+  if (event.keyCode === 13) {
+
+    socket.emit('chat2', {
         message: message.value,
         handle: handle.value
     });
-    message.value = "";
-});
+      message.value="";
+    // Trigger the button element with a click
+    document.getElementById('btn').click();
 
-message.addEventListener('keypress', function(){
-    socket.emit('typing', handle.value);
-})
+
+  }
+});
 
 // Listen for events
 socket.on('chat', function(data){
-    feedback.innerHTML = '';
+    feedback.innerHTML = "";
     output.innerHTML += '<p><strong>' + data.handle + ': </strong>' + data.message + '</p>';
 });
 
-socket.on('typing', function(data){
-    feedback.innerHTML = '<p><em>' + data + ' is typing a message...</em></p>';
+socket.on('chat2', function(data){
+    feedback.innerHTML = "";
+    output.innerHTML += '<p><strong>' + data.handle + ': </strong>' + data.message + '</p>';
+});
+
+
+socket.on('typing',function(data){
+  feedback.innerHTML ='<p><em>'+data+' is typing a message...</em></p>'
 });
